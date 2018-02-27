f7fb0035ab9a041a4883c763ecc0abe849cc4188
Peter Donner <peter@markup.at>
2018-02-27 17:03:51 +0000

    Only compile SASS when there is a diff.

--- src/at/markup/boot.clj
+++ src/at/markup/boot.clj
@@ -35,42 +35,42 @@
 
 (defn has-anything-changed? [last-fileset fileset]
   (println "has-anything-changed?")
-  (let [diff (c/by-ext [".scss"] (c/input-files (c/fileset-diff last-fileset fileset :hash)))]
-    (println diff)
-    true))
+  (let [diff (c/by-ext [".scss"] (c/input-files (c/fileset-diff last-fileset fileset :hash)))
+        has-diff (= () diff)]
+    (not has-diff)))
 
 (c/deftask sass []
-  (let [tmp (c/tmp-dir!)
-        tmp-with-webchars (c/tmp-dir!)
+  (let [tmp-css-resource (c/tmp-dir!)
+        tmp-scss (c/tmp-dir!)
         last-fileset (atom nil)]
+    ;; copy SCSS files from webjars
+    (doseq [[k v] (asset-map)
+            :when (.endsWith k ".scss")]
+      (let [content (slurp (io/resource v))
+            out-file (io/file tmp-scss k)]
+        (doto out-file io/make-parents (spit content))))
     (fn middleware [next-handler]
       (fn handler [fileset]
-        (if (has-anything-changed? @last-fileset fileset)
-          (do (c/empty-dir! tmp)
-              ;; copy SCSS files from webjars
-              (doseq [[k v] (asset-map)]
-                (if (.endsWith k ".scss")
-                  (let [content (slurp (io/resource v))
-                        out-file (io/file tmp-with-webchars k)]
-                    (doto out-file io/make-parents (spit content)))))
-              (let [in-files (c/input-files fileset)
-                    scss-files (c/by-ext [".scss"] in-files)]
-                ;; copy SCSS files from resources
-                (doseq [in scss-files]
-                  (let [in-file (c/tmp-file in)
-                        in-path (c/tmp-path in)
-                        out-file (io/file tmp-with-webchars in-path)]
-                    (doto out-file io/make-parents (spit (slurp in-file)))))
-                ;; compile files that do not start with underscores
-                (doseq [in scss-files]
-                  (let [in-file (c/tmp-file in)
-                        in-path (c/tmp-path in)
-                        out-file (io/file tmp (scss->css in-path))]
-                    (if (not (.startsWith (.getName in-file) "_"))
-                      (doto out-file io/make-parents
-                            (spit (compile-sass in-path (str tmp-with-webchars)))))))
-                (let [new-fileset (c/commit! (c/add-resource fileset tmp))]
-                  (reset! last-fileset new-fileset)
-                  (println "new-fileset" new-fileset)
-                  (next-handler new-fileset))))
-          (next-handler fileset))))))
+        (when (has-anything-changed? @last-fileset fileset)
+          (let [in-files (c/input-files fileset)
+                scss-files (c/by-ext [".scss"] in-files)]
+            ;; copy SCSS files from resources
+            (doseq [in scss-files]
+              (let [in-file (c/tmp-file in)
+                    in-path (c/tmp-path in)
+                    out-file (io/file tmp-scss in-path)]
+                (doto out-file io/make-parents (spit (slurp in-file)))))
+            ;; compile files that do not start with underscores
+            (doseq [in scss-files]
+              (let [in-file (c/tmp-file in)
+                    in-path (c/tmp-path in)
+                    out-file (io/file tmp-css-resource (scss->css in-path))]
+                (if (not (.startsWith (.getName in-file) "_"))
+                  (doto out-file io/make-parents
+                        (spit (compile-sass
+                               in-path
+                               (str tmp-scss)))))))))
+        (let [new-fileset (c/commit! (c/add-resource fileset tmp-css-resource))]
+          (reset! last-fileset new-fileset)
+          (println "new-fileset" new-fileset)
+          (next-handler new-fileset))))))
