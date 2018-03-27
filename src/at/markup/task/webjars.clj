f33f99ab21e26d64c1fb8ab6c281840168498731
Peter Donner <peter@markup.at>
2018-03-27 21:01:51 +0000

    Multimethod to handle 'webjars' task.

--- src/at/markup/task/webjars.clj
+++ src/at/markup/task/webjars.clj
@@ -5,13 +5,23 @@
    [at.markup.webjars :refer [asset-map]]))
 
 (s/def ::webjars-cmd-arg
-  #{"list"})
+  #{"ls"})
 
+(defn nodes []
+  (sort (filter #(not (= ["/"] %))
+                (map (fn [x] [x]) (keys (asset-map))))))
+
+(defmulti cmd-handler (fn [[m]] m))
+
+(defmethod cmd-handler "ls" [cmd-arg]
+  (print-tree [["webjars" (nodes)]]))
+
+(defmethod cmd-handler "export" [cmd-arg]
+  (println "export handler"))
+  
 (defn webjars
-  [[cmd]]
+  [args]
   (fn middleware [next-handler]
     (fn handler [fileset]
-      (let [nodes (sort (filter #(not (= ["/"] %))
-                                (map (fn [x] [x]) (keys (asset-map)))))]
-        (print-tree [["webjars" nodes]])
-        (next-handler fileset)))))
+      (cmd-handler args)
+      (next-handler fileset))))
