2f221069a739a089e19043990a904f9db61148e0
Peter Donner <peter@markup.at>
2018-04-06 22:14:36 +0000

    Implement 'export' argument for 'webjars' task.

--- src/at/markup/task/webjars.clj
+++ src/at/markup/task/webjars.clj
@@ -1,19 +1,21 @@
 (ns at.markup.task.webjars
   (:require
+   [boot.core :as c]
+   [clojure.java.io :as io]
    [clojure.spec.alpha :as s]
    [boot.util :refer [print-tree]]
    [at.markup.webjars :refer [asset-map]]))
 
 (s/def ::webjars-cmd-arg
-  #{"ls"})
+  #{"ls" "export"})
 
 (defn nodes []
   (sort (filter #(not (= ["/"] %))
                 (map (fn [x] [x]) (keys (asset-map))))))
 
-(defmulti cmd-handler (fn [[m]] m))
+(defmulti cmd-handler (fn [[m] _] m))
 
-(defmethod cmd-handler "ls" [[_ regex]]
+(defmethod cmd-handler "ls" [[_ regex] fileset]
   (let [regex-nil? (nil? regex)
         label (if regex-nil? "webjars" (str "webjars filter=(" regex ")"))
         nodes (if regex-nil?
@@ -21,14 +23,27 @@
                 (filter #(re-find
                           (re-pattern regex) (first %))
                         (nodes)))]
-    (print-tree [[label nodes]])))
+    (print-tree [[label nodes]]))
+  fileset)
 
-(defmethod cmd-handler "export" [cmd-arg]
-  (println "export handler"))
+(defmethod cmd-handler "export" [[_ regex] fileset]
+  (let [regex-nil? (nil? regex)
+        nodes (if regex-nil?
+                (nodes)
+                (filter #(re-find
+                          (re-pattern regex) (first %))
+                        (nodes)))
+        assets (asset-map)
+        tmp-dir (c/tmp-dir!)]
+    (doseq [node nodes]
+      (let [v (get assets (first node))
+            content (slurp (io/resource v))
+            out-file (io/file tmp-dir (first node))]
+        (doto out-file io/make-parents (spit content))))
+    (c/commit! (c/add-resource fileset tmp-dir))))
 
 (defn webjars
   [args]
   (fn middleware [next-handler]
     (fn handler [fileset]
-      (cmd-handler args)
-      (next-handler fileset))))
+      (next-handler (cmd-handler args fileset)))))
